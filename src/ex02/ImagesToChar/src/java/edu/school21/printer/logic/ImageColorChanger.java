28df59b922acd0964f809506abe61b8418bfc2fe
Darina Ayurova <comedial@student.21-school.ru>
2023-10-02 21:21:27 +0300

    02 done

--- src/ex02/ImagesToChar/src/java/edu/school21/printer/logic/ImageColorChanger.java
+++ src/ex02/ImagesToChar/src/java/edu/school21/printer/logic/ImageColorChanger.java
@@ -10,12 +10,14 @@ import com.diogonunes.jcdp.color.api.Ansi;
 
 public class ImageColorChanger {
     private static final String IMAGE = "src/resources/it.bmp";
+    private static final int HEIGHT = 16;
+    private static final int WIDTH = 16;
 
     public static void printImage(String whiteColor, String blackColor) throws IOException {
         BufferedImage image = ImageIO.read(new File(IMAGE));
 
-        for (int y = 0; y < 16; y++) {
-            for (int x = 0; x < 16; x++) {
+        for (int y = 0; y < HEIGHT; y++) {
+            for (int x = 0; x < WIDTH; x++) {
                 String colorCode = (image.getRGB(x, y) == Color.BLACK.getRGB()) ? blackColor : whiteColor;
                 ColoredPrinter colorPrinter = new ColoredPrinter.Builder(1, false)
                         .background(Ansi.BColor.valueOf(colorCode))
